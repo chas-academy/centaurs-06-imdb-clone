742ad5a6c51bb9fb24440a12b257f38940b111ce
Andreas <Andreas@admins-MacBook-Pro.local>
2017-11-30 15:21:41 +0100

    Initial structure for Laravel/Foundation

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,14 @@
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Laravel {{ app()->version() }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout')
+@section('content')
+<div class="grid-container">
+    <div class="grid-x grid-margin-x">
+        <h1>Centaurs IMDb-clone</h1>
+    </div>
+  </div>
+@endsection
