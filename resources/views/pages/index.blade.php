b3c46fa1d8ca0b209965101474e616132bbecb99
Victor Ciavarella <Ciavarella@Victors-MacBook-Pro.local>
2017-12-04 13:26:58 +0100

    made buttons with paths to api-testing

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,5 +4,8 @@
     <div class="grid-x grid-margin-x">
         <h1>Centaurs IMDb-clone</h1>
     </div>
+        <a href="/test" class="button">Test</a>
+        <a href="/actors" class="button">Actors</a>
+        <a href="/movies" class="button">Movies</a>
   </div>
 @endsection
