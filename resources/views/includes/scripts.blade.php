2fc20c8fb29ed320d2f5a7d09bbd4f37365a96dd
patryk7rybaczek <patryk.rybaczek@chasacademy.se>
2017-12-05 14:05:45 +0100

    Header for mobile WIP

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,7 +1,9 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/awesome.js') }}"></script>
     
     <!-- Foundation init -->
     <script>
         $(document).foundation();
     </script>
+    
