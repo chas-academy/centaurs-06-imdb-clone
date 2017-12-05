768393cae9239ab56c59419252c33e8545843066
Victor Ciavarella <Ciavarella@Victors-MacBook-Pro.local>
2017-12-05 15:27:59 +0100

    added section with placeholders for posters

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,4 +5,40 @@
         <i class="fa fa-search" aria-hidden="true"></i>
     </header>
 </div>
+
+
+
+
+        <!-- Section with three small movieposters -->
+
+<section class="grid-x" class="row" style="margin-top:20px;">
+    <div class="row">
+        <div>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        </div>
+        <img src="http://via.placeholder.com/80x150">
+    </div>
+
+    <div class="row">
+        <div>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        </div>
+        <img src="http://via.placeholder.com/80x150">
+    </div>
+
+    <div class="row">
+        <div>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        </div>
+        <img src="http://via.placeholder.com/80x150">
+    </div>
+</section>
+
+        <!-- Section with one big poster -->
+<section class="grid-x" style="margin-top:20px;">
+    <div class="row">
+    <img src="http://via.placeholder.com/290x400">
+    <div>
+</section>
+
 @endsection
