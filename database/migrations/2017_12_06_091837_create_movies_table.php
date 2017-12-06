325abf151f851266d8f80858c46ab58970e3c272
Andreas <Andreas@admins-MacBook-Pro.local>
2017-12-06 13:57:24 +0100

    added timespams property to several tables

---
+++ database/migrations/2017_12_06_091837_create_movies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('plot');
+            $table->integer('playtime');
+            $table->lineString('poster');
+            $table->lineString('backdrop');
+            $table->date('releasedate');
+            $table->integer('imdb_rating');
+            $table->integer('chas_rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
