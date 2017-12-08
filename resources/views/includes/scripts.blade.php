bf1e5bee3c9e77d6eeaf8319d83633cbc8cc8dc5
patryk7rybaczek <patryk.rybaczek@chasacademy.se>
2017-12-08 14:10:01 +0100

    Changed some classes and id's to make the code more readable

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -6,8 +6,43 @@
     <script>
         $(document).foundation();
             // Select 2 script foundation
-        $(document).ready(function() {
+        $(document).ready(function() 
+        {
             $('.js-example-basic-single').select2();
+        });
+        $(document).ready(function() 
+        {
+            // Mobile-animations 
+            $('#sign-in').click( function(e){
+                $('#mobile-btn-wrap').toggle();
+                $('#mobile-btn-back').css('display', 'flex');
+                $('#sign-in-f').css('display', 'block');
+                if($('#sign-in-f').css('display') == 'block')
+                {
+                    $('#mobile-btn-quit').hide();
+                    $('#mobile-btn-back').click (function(e){
+                        $('#mobile-btn-quit').show();
+                        $('#mobile-btn-back').hide();
+                        $('#sign-in-f').css('display', 'none');
+                        $('#mobile-btn-wrap').css('display', 'block');
+                    });
+                }               
+            });
+            $('#sign-up').click( function(e){
+                $('#mobile-btn-wrap').toggle();
+                $('#mobile-btn-back').css('display', 'flex');
+                $('#sign-up-f').css('display', 'block');
+                if($('#sign-up-f').css('display') == 'block')
+                {
+                    $('#mobile-btn-quit').hide();
+                    $('#mobile-btn-back').click (function(e){
+                        $('#mobile-btn-quit').show();
+                        $('#mobile-btn-back').hide();
+                        $('#sign-up-f').css('display', 'none');
+                        $('#mobile-btn-wrap').css('display', 'block');
+                    });
+                }
+            });
         });
     </script>
     
