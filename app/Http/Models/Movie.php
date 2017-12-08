ce57a43dd4700c718ecc227e7e4e628277398a63
Andreas <Andreas@admins-MacBook-Pro.local>
2017-12-08 17:52:09 +0100

    created function for 'getMovie', 'createGenres' and 'getGenres' also added 'genre creation' to 'createMovie' function

--- app/Http/Models/Movie.php
+++ app/Http/Models/Movie.php
@@ -4,6 +4,7 @@ namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+
 use DB;
 
 class Movie extends Model
@@ -23,6 +24,17 @@ class Movie extends Model
             'created_at' => now(),
             'updated_at' => now()
         ]);
+
+        $movie = $this->getMovie($properties['results'][0]['title']);
+        $genres = $this->getGenres($properties['results'][0]['genre_ids']);
+
+        foreach ($genres as $genre) {
+
+            DB::table('ledger_genres')->insert([
+                'movie_id' => $movie->id,
+                'genre_id' => $genre->id
+            ]);
+        }
     }
 
     public function createMovieStaff($properties) 
@@ -48,5 +60,33 @@ class Movie extends Model
                         ]);    
                 }
             }            
+    }
+
+    public function createMovieGenres($properties) 
+    {
+        foreach ($properties['genres'] as $genre) {
+            DB::table('genres')->insert([
+                'genre_name' => $genre['name'],
+                'api_genre_id' => $genre['id']
+            ]);
+        }             
+    }
+
+    public function getMovie($movieTitle) 
+    {
+        $movie = DB::table('movies')->get()->where('title', $movieTitle);
+
+        return array_first($movie);
+    }
+
+    public function getGenres($genreId)
+    {
+        $genres = [];
+
+        foreach ($genreId as $id) {
+            array_push($genres , DB::table('genres')->get()->where('api_genre_id', $id)->first());
+        }
+            
+        return $genres;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', function () {
 });
 
 Route::get('/movietest', 'MovieController@SearchForMovie');
+
+Route::get('/creategenres', 'MovieController@getMovieGenres');
