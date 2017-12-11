ed4a0be80fb6dd437bc1bc0833485e2da6441e0d
patryk7rybaczek <patryk.rybaczek@chasacademy.se>
2017-12-11 16:17:56 +0100

    Current movie view done, form styling on sign in/up done.

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('pages.index');
 });
 
-Route::get('/login', function () {
-    return view('pages.login');
-});
-
-Route::get('/create', function () {
-    return view('pages.create');
+Route::get('movie', function (){
+    return view('pages.movie');
 });
