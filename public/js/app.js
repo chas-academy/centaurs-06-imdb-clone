1574bb7cbdf73ee7e55f5baae26ec3f055dc152e
patryk7rybaczek <patryk.rybaczek@chasacademy.se>
2017-12-11 16:48:26 +0100

    Deleted useless import in app.scss

--- public/js/app.js
+++ public/js/app.js
@@ -41110,7 +41110,7 @@ module.exports = function spread(callback) {
 /* 36 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n@import \"styles/footer\";\n^\n      File to import not found or unreadable: /Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/resources/assets/sass/styles/footer.sass.\nParent style sheet: stdin\n      in /Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/resources/assets/sass/app.scss (line 20, column 1)\n    at runLoaders (/Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/async/dist/async.js:958:16)\n    at options.error (/Users/Andreas/Documents/chas-academy/06-centaurs-imdb-clone/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
