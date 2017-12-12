3013fe268a84246f372816b67f8fc9c3c90bee69
Andreas <Andreas@admins-MacBook-Pro.local>
2017-12-12 10:44:52 +0100

    temporary commented out deploy paths

--- deploy.php
+++ deploy.php
@@ -29,19 +29,19 @@ host('165.227.236.122')
     ->stage('develop')
     ->set('branch', 'develop');
 
-host('165.227.236.122')
-    ->set('deploy_path', '/var/www/staging.centaurs-imdb.me')   
-    ->user('deployer')
-    ->identityFile('~/.ssh/id_rsa')
-    ->stage('staging')
-    ->set('branch', 'staging');
+// host('165.227.236.122')
+//     ->set('deploy_path', '/var/www/staging.centaurs-imdb.me')   
+//     ->user('deployer')
+//     ->identityFile('~/.ssh/id_rsa')
+//     ->stage('staging')
+//     ->set('branch', 'staging');
 
-host('165.227.236.122')
-    ->set('deploy_path', '/var/www/www.centaurs-imdb.me')   
-    ->user('deployer')
-    ->identityFile('~/.ssh/id_rsa')
-    ->stage('master')
-    ->set('branch', 'master');
+// host('165.227.236.122')
+//     ->set('deploy_path', '/var/www/www.centaurs-imdb.me')   
+//     ->user('deployer')
+//     ->identityFile('~/.ssh/id_rsa')
+//     ->stage('master')
+//     ->set('branch', 'master');
 // Tasks
 
 task('build', function () {
