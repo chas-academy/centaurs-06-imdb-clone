65bb7dfe8560245ac0b6196cf4cad99f4f779c0d
Andreas <Andreas@admins-MacBook-Pro.local>
2017-12-15 16:23:59 +0100

    updated deploy.php - again

--- deploy.php
+++ deploy.php
@@ -40,22 +40,9 @@ host('165.227.236.122')
     ->set('deploy_path', '/var/www/develop.centaurs-imdb.me')   
     ->user('deployer')
     ->identityFile('~/.ssh/id_rsa')
-    ->stage('production')
-    ->set('branch', 'master');
-
-host('165.227.236.122')
-    ->set('deploy_path', '/var/www/staging.centaurs-imdb.me')   
-    ->user('deployer')
-    ->identityFile('~/.ssh/id_rsa')
-    ->stage('staging')
-    ->set('branch', 'staging');
-
-host('165.227.236.122')
-    ->set('deploy_path', '/var/www/develop.centaurs-imdb.me')   
-    ->user('deployer')
-    ->identityFile('~/.ssh/id_rsa')
     ->stage('develop')
     ->set('branch', 'develop');
+    
 // Tasks
 
 task('build', function () {
