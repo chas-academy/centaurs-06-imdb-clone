350ba2afcc101b41d97556f4ea098ff6588ad2a8
Pontus Särland <pontus.sarland@chasacademy.se>
2017-12-20 14:52:19 +0100

    Now also add actors and actor ledgers for tvshows from api

--- app/Http/Controllers/TvShowController.php
+++ app/Http/Controllers/TvShowController.php
@@ -31,36 +31,18 @@ class TvShowController extends Controller
 
     public function createTvShowFromApi()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         $keyword = "Gam of thrones";
-=======
-        $keyword = "Family guy";
->>>>>>> methods for adding tv-show seasons from api
-=======
-        $keyword = "Gam of thrones";
->>>>>>> can now add tvShow, season, and episode from api
         $argument = str_replace(' ', '%20', $keyword);
         $searchMethod = 'search/tv?';
         $search = '&language=en-US&query=' . $argument . '&page=1';
 
         $result = $this->TvShowApi($search, $searchMethod);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> methods for adding tv-show seasons from api
         
         $tvShowModel = new TvShow();
         $tvShowModel->createTvSHowFromApi($result['results'][0]);
 
         $seasons = $this->getTvShowSeasons($result['results'][0]);
         $tvShow = $tvShowModel->getTvShowByName($seasons['name']);
-<<<<<<< HEAD
-=======
-        //print_r($result);
-        $tvShow = new TvShow();
-        $tvShow->createTvSHowFromApi($result['results'][0]);
->>>>>>> Made som changes in db tables for tvshows and episodes
 
         foreach ($seasons['seasons'] as $k => $season) {
             if($k <> 0) { 
@@ -98,55 +80,5 @@ class TvShowController extends Controller
 
         return $this->TvShowApi($languageEndString, $searchMethod);
     }
-<<<<<<< HEAD
 }
 
-=======
-    public function getTvShowStaffFromApi($tvShow)
-=======
-
-        foreach ($seasons['seasons'] as $k => $season) {
-            if($k <> 0) { 
-                $tvShowModel->createSeasonsFromApi($season, $tvShow);
-                
-                for ($i=1; $i <= $season['episode_count']; $i++) { 
-                    $episodeInfo = $this->getEpisodeInfoFromApi([$seasons][0]['id'], $season['season_number'], $i);
-                    $episodeCredits = $this->getEpisodeActorsFromApi([$seasons][0]['id'], $season['season_number'], $i);
-                    $tvShowModel->createEpisodeFromApi($episodeInfo, $episodeCredits, $tvShow->id, $seasons);
-                } 
-            }
-        }
-    }
-
-    public function getEpisodeActorsFromApi($tvShowId, $seasonNr, $episodeNr)
-    {
-        $searchMethod = 'tv/' . $tvShowId . '/' . 'season/' . $seasonNr . '/' . 'episode/' . $episodeNr . '/credits?';
-        $languageEndString = '&language=en-US';
-
-        return $this->tvShowApi($languageEndString, $searchMethod);
-    }
-
-    public function getEpisodeInfoFromApi($tvShowId, $seasonNr, $episodeNr)
->>>>>>> methods for adding tv-show seasons from api
-    {
-        $searchMethod = 'tv/' . $tvShowId . '/' . 'season/' . $seasonNr . '/' . 'episode/' . $episodeNr . '?';
-        $languageEndString = '&language=en-US';
-
-        return $this->tvShowApi($languageEndString, $searchMethod);
-    }
-    public function getTvShowSeasons($tvShow)
-    {
-        $searchMethod = 'tv/' . $tvShow['id'] . '?';
-        $languageEndString = '&language=en-US';
-
-        return $this->TvShowApi($languageEndString, $searchMethod);
-    }
-}
-<<<<<<< HEAD
-                               //"tv/1396/season/0/episode/1?"
-//https://api.themoviedb.org/3/tv/1399/season/1/episode/1?api_key=6975fbab174d0a26501b5ba81f0e0b3c&language=en-US
-//https://api.themoviedb.org/3/tv/1399?api_key=6975fbab174d0a26501b5ba81f0e0b3c&language=en-US
->>>>>>> Made som changes in db tables for tvshows and episodes
-=======
-
->>>>>>> can now add tvShow, season, and episode from api
