7c2ac556ed2951a827fdd6c03bdb61996523da2b
Andreas <Andreas@admins-MacBook-Pro.local>
2018-01-04 13:05:48 +0100

    added migration for ledgerWatchList

---
+++ database/migrations/2018_01_04_111426_create_ledger_watch_lists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLedgerWatchListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ledger_watch_lists', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('movie_id')->unsigned()->nullable();
+            $table->integer('episode_id')->unsigned()->nullable();
+        });
+        
+        Schema::table('ledger_watch_lists', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->foreign('episode_id')->references('id')->on('episodes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ledger_watch_lists');
+    }
+}
