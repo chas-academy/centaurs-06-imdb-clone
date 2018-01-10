55624bfa26e3c4b405d17e84ddfad6bbe660841e
Ida Englund <ida.englund@chasacademy.se>
2018-01-10 16:44:03 +0100

    Add more movies to Algolia

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -44,7 +44,7 @@ class MovieController extends Controller
         
         public function createMovieFromApi() 
         {
-            $keyword = "bond";
+            $keyword = "Air force one";
             $argument = str_replace(' ', '%20', $keyword);
             $searchMethod = 'search/movie?';
             $search = '&language=en-US&query=' . $argument . '&page=1&include_adult=false';
