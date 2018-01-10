f630b99c301be0a73647fc27398a28d94bfea331
Victor Ciavarella <Ciavarella@Victors-MacBook-Pro.local>
2018-01-10 12:36:22 +0100

    fixed redirect when creating user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -21,7 +21,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/profile';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use Image;
+use View;
 
 class UserController extends Controller
 {
