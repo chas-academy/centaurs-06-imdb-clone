3f1b8fab829e199d3aef6b1c0574865e427b7c9b
Laya Sadegh <laya.sadegh@chasacademy.se>
2018-01-14 19:41:09 +0100

    Add errormessagesto form

--- resources/views/pages/createmovie.blade.php
+++ resources/views/pages/createmovie.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin') 
 @section('content')
 
-<form class="adminform" action="" method="post" enctype="multipart/form-data">
+<form data-abide novalidate class="adminform" action="" method="post" enctype="multipart/form-data">
     <div class="small-12 columns">
     <h1>Create movie</h1>
 
@@ -17,62 +17,78 @@
 
     <?php echo csrf_field(); ?>
 
-    <input type="text" name="title" placeholder="Movie title">
+    <label>
+        <input type="text" name="title" placeholder="Movie title" required>
+        <span class="form-error">
+        Please fill in movie title.
+        </span>
+    </label>
 
     <label for="uploadPoster" class="button">Upload poster</label>
-    <input type="file" name="poster" id="uploadPoster" class="show-for-sr" required/>
+        <input type="file" name="poster" id="uploadPoster" class="show-for-sr" required/>
+        <span class="form-error">
+        Please add movie poster.
+        </span>
+    </label>
 
     <label> 
-        <select multiple class="multi-select" name="genre">
+        <select multiple class="multi-select" name="genre" required>
         @foreach ($genres as $genre)
             <option value="{{ $genre["id"] }}">{{ $genre["genre_name"] }}</option>
         @endforeach
         </select>
+        <span class="form-error">
+        Don't forget to add genre.
+        </span>
     </label> 
     
         <!-- <select class="js-example-basic-single" name="releaseyear"> -->
+        <!-- Add placeholder -->
     <label>
-    <select multiple class="release-year" name="genre">
+        <select class="release-year" name="releaseyear" required>
         @foreach ($releaseyears as $releaseyear)
             <option value="{{ $releaseyear }}">{{ $releaseyear }}</option>
         @endforeach
         </select>
+        <span class="form-error">
+        Add release year. 
+        </span>
     </label>
 
     <label>
-        <input type="number" name="playtimeMins" placeholder="Playtime minutes">
+        <input type="number" name="playtimeMins" placeholder="Playtime minutes" required>
+        <span class="form-error">
+        Add playtime. 
+        </span>
     </label>
 
     <label>
-        <textarea cols="30" rows="10" placeholder="Movie plot"></textarea>
+        <textarea cols="30" rows="10" placeholder="Movie plot" required></textarea>
+        <span class="form-error">
+        Don't forget to write the movie plot.
+        </span>
     </label>
 
         @include('partials.personlist', ['persons' => $actors, 'personType' => 'actor'])
         @include('partials.personlist', ['persons' => $directors, 'personType' => 'director'])
         @include('partials.personlist', ['persons' => $producers, 'personType' => 'producer'])
         
-        <button class="button" type="submit" value="BOBBY!">Create Movie</button>
+        <button class="button" type="submit" value="submit">Create Movie</button>
     
     </div>
 </form>
     
 @endsection 
 
+
 @section('page-scripts')
-
 <script>
+    $(document).ready(function() {
+        // Select 2 script foundation
+        $('.multi-select').select2({
+            placeholder: "Choose Genre"
+        });
 
-$(document).ready(function() {
-    // Select 2 script foundation
-    $('.multi-select').select2({
-        placeholder: "Choose Genre"
     });
-
-    $('.release-year').select2({
-        placeholder: "Release Year"
-    }); 
-});
-
 </script>
-
 @endsection
--- resources/views/partials/personlist.blade.php
+++ resources/views/partials/personlist.blade.php
@@ -1,5 +1,5 @@
-<div class="js-personlist" data-field="{{$personType}}" data-initial='{!! json_encode($initial ?? []) !!}'>
-    <h3>{{$personType}}s</h3>
+<div class="js-personlist" data-field="{{$personType}}">
+    <!--<h3 style="color: white;">{{$personType}}s</h3> -->
 
     <div>
         <lable style="color: white;">Existing {{$personType}}</lable>
@@ -7,23 +7,26 @@
    
     <div>
         <label>
-        <select name="state">
+        <select multiple class="multi-select" id="add-cast" name="state">
           @foreach ($persons as $person)
             <option value="{{ $person["id"] }}">{{ $person["name"] }}</option>
           @endforeach
         </select>
-        <button class="button secondary js-personlist-existing-person-add">Add</button>
+        <!--<button class="button secondary js-personlist-existing-person-add">+</button> -->
         </label>
     </div>
 
+
     <div>
-        <lable style="color: white;">New {{$personType}}</lable>
-        <input type="text" name="humhum" class="js-personlist-new-person-field">
-        <button class="button secondary js-personlist-new-person-add">Add</button>
+        <!-- <lable style="color: white;">New {{$personType}}</lable> -->
+        <input type="text" name="humhum" class="js-personlist-new-person-field" placeholder="Add">
+        <button class="button secondary js-personlist-new-person-add">+</button>
     </div>
 
     <div>
       <ul class="js-personlist-choices">
       </ul>
     </div>
+
 </div>
+
