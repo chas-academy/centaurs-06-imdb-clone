e9833108954e15071f0276ea82dccf3dec970b4d
Laya Sadegh <laya.sadegh@chasacademy.se>
2018-01-15 15:16:31 +0100

    Added styling for button

--- resources/views/partials/personlist.blade.php
+++ resources/views/partials/personlist.blade.php
@@ -1,6 +1,5 @@
 <div class="js-personlist" data-field="{{$personType}}">
 
-   
     <div>
     <select class="js-example-placeholder-multiple js-states form-control js-personlist-existing-person-chooser {{$personType}}" multiple="multiple" name="state">
          @foreach ($persons as $person)
@@ -9,10 +8,9 @@
        </select>
     </div>
 
-
-    <div>
-    <input type="text" name="humhum" placeholder="Add a new {{$personType}}" class="js-personlist-new-person-field">
-        <button id="add-button" class="button secondary js-personlist-new-person-add">+</button>
+    <div id="add-person">
+    <input type="text" name="text" placeholder="Add a new {{$personType}}" class="js-personlist-new-person-field">
+    <button id="person-btn" class="secondary js-personlist-new-person-add"><i class="fa fa-plus-circle fa-2x" aria-hidden="true"></i></button>
     </div>
 
     <div>
