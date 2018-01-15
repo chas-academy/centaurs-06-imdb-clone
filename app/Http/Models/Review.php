b3339d27b1d441d192c1c5c2971d2fcddf97f643
Andreas-sjolund-chas <andreas.sjolund@chasacademy.se>
2018-01-15 14:19:23 +0100

    added function for creating a review

--- app/Http/Models/Review.php
+++ app/Http/Models/Review.php
@@ -1,9 +1,29 @@
 <?php
 
-namespace App;
+namespace App\Http\Models;
 
+use DB;
+use Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    public function createReview($request, $movieId)
+    {
+        $userId = Auth::User()->id;
+        $rating = $request->get('rating');
+        $title = $request->get('title');
+        $content = $request->get('content');
+
+        DB::table('reviews')->insert([
+            'user_id' => $userId,
+            'movie_id' => $movieId,
+            'episode_id' => null,
+            'title' => $title,
+            'content' => $content,
+            'review_rating' => $rating,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
 }
