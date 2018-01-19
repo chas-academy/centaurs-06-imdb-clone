18fac8b19bfe05113fded70de44aca74d1c20d82
Laya Sadegh <laya.sadegh@chasacademy.se>
2018-01-19 19:07:49 +0100

    Add styling to delete movie. Change position includeds in blades

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,12 +2,10 @@
 <div class="off-canvas-content" data-off-canvas-content>
     @include('includes.header')
     @include('includes.messages')
-        @include('includes.errors')
+    @include('includes.errors')
 <main class="row">
     @include('includes.menu-btn')
         <section class="small-12">
-            @include('includes.messages')
-            @include('includes.errors')
         </section>
         <div class="small-12 tv-shows">
             <a id="movies-link" href="/">Movies</a>
