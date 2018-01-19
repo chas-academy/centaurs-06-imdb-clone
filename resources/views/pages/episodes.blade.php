cb2bdba8cc900bfa7b8b105ce26514a82347b786
Andreas-sjolund-chas <andreas.sjolund@chasacademy.se>
2018-01-19 11:23:21 +0100

    removed review buttons from seasons/episode pages of tvshows

--- resources/views/pages/episodes.blade.php
+++ resources/views/pages/episodes.blade.php
@@ -3,7 +3,6 @@
 $i = $_GET['episode'];
 $j = $_GET['season'];
 $currentEpisode = 'Episode-' . $i;
-// dd($episodeDetails)
 ?>
 
 
@@ -130,44 +129,27 @@ $currentEpisode = 'Episode-' . $i;
                 </div>
             </div>
         </div>
-            <div class="small-12 btn-container">
-                <button class="read-review" data-toggle="read-reviews">Read reviews</button>
-
-                @if(Auth::check())
-                    <button class="writer-review" data-toggle="write-reviews">Write a review</button>
-                @endif
-            </div> 
-        </div>
     </div>
-    <div class="small-12 review-flex-align">
-        <div id="write-reviews" class="write-reviews" data-toggler=".visible">
-            <div class="review-rate">
-                <form class="write-review-form" action="/addreview" method="POST">
-                    {{ csrf_field() }}
-                    <fieldset class="rating">
-                        <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-                        <input type="radio" id="star4half" name="rating" value="4.5" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
-                        <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-                        <input type="radio" id="star3half" name="rating" value="3.5" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
-                        <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-                        <input type="radio" id="star2half" name="rating" value="2.5" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
-                        <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-                        <input type="radio" id="star1half" name="rating" value="1.5" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
-                        <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
-                        <input type="radio" id="starhalf" name="rating" value="0.5" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
-                    </fieldset>
-            </div>
-                <input type="text" placeholder="Title" name="title">
-                <textarea name="content" id="" cols="30" rows="20" placeholder="Content of review"></textarea>
-                <button class="send-review" type="submit" name=''>Add review</button>
-            </form>                   
-        </div>
-                <div id="review-1" class="small-12 review">
-                    <div class="small-12 review-rate">
-                        <p>No reviews written for this movie yet.</p>
+</div>
+                <div class="small-12 review-flex-align">
+                    <div id="write-reviews" class="write-reviews" data-toggler=".visible">
+                        <div class="review-rate">
+                            <form class="write-review-form" action="/addreview" method="POST">
+                                {{ csrf_field() }}
+                                <fieldset class="rating">
+                                    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                                    <input type="radio" id="star4half" name="rating" value="4.5" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                                    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                                    <input type="radio" id="star3half" name="rating" value="3.5" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                                    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                                    <input type="radio" id="star2half" name="rating" value="2.5" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                                    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                                    <input type="radio" id="star1half" name="rating" value="1.5" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                                    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+                                    <input type="radio" id="starhalf" name="rating" value="0.5" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                                </fieldset>
+                        </div>              
                     </div>
-                </div>
-            
                 </div>
             </div>
         </div>
